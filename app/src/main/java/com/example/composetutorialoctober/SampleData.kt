ee1ae7343aa7477e6508b10dd651c8043332c9c8
Sehar Batool <97355349+seharbat00l@users.noreply.github.com>
2022-10-23 23:56:28 +0500

    first

---
+++ app/src/main/java/com/example/composetutorialoctober/SampleData.kt
@@ -0,0 +1,75 @@
+package com.example.composetutorialoctober
+
+/**
+ * SampleData for Jetpack Compose Tutorial 
+ */
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Sehar Batool",
+            "Test...Test...Test..."
+        ),
+        Message(
+            "Lexi",
+            """List of Android versions:
+            |Android KitKat (API 19)
+            |Android Lollipop (API 21)
+            |Android Marshmallow (API 23)
+            |Android Nougat (API 24)
+            |Android Oreo (API 26)
+            |Android Pie (API 28)
+            |Android 10 (API 29)
+            |Android 11 (API 30)
+            |Android 12 (API 31)""".trim()
+        ),
+        Message(
+            "Lexi",
+            """I think Kotlin is my favorite programming language.
+            |It's so much fun!""".trim()
+        ),
+        Message(
+            "Sehar Batool",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "Sehar Batool",
+            """Hey, take a look at Jetpack Compose, it's great!
+            |It's the Android's modern toolkit for building native UI.
+            |It simplifies and accelerates UI development on Android.
+            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trim()
+        ),
+        Message(
+            "Sehar Batool",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "Sehar Batool",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "Sehar Batool",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "Sehar Batool",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "Sehar Batool",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "Sehar Batool",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Sehar Batool",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Sehar Batool",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
